b15d2c2e6a742ebc65d68c1e8cac4393c03a613f
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-10 11:52:13 -0600

    01_Update component template and component style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `Hello Universe`,
+  styles: `
+    :host {
+      color: #a144eb;
+    }
+  `
 })
 export class AppComponent {
   title = 'Ang_01_LAYB';
