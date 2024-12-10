e2263c4b2d425f2d211822678df3dc26dd94842f
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-10 13:30:18 -0600

    03_Composing Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `Hello {{ city }}, {{ 1 + 1 }}`,
-  styles: `
-    :host {
-      color: #a144eb;
-    }
+  selector: 'app-user',
+  template: `
+    Username: {{ username }}
   `
 })
+
+export class UserComponent {
+  username = 'YoungTech'
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <section>
+      <app-user />
+    </section>
+  `,
+})
+
 export class AppComponent {
   title = 'Ang_01_LAYB';
-  city = 'San Francisco';
 }
