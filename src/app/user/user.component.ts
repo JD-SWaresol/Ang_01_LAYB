c362444bdbc3fad5adda23667bef635e46891065
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-13 12:39:00 -0600

    16_Form Control Values

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,19 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-user',
   template: `
     <p>Username: {{ username }}</p>
-    <!-- Muestra el Framework que ingresemos en el campo de texto -->
-    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <p>Framework: {{ favoriteFramework }}</p>
     <label for="framework">
-      <!-- Recibe el nombre tecleado del Framework -->
       Favorite Framework:
       <input id="framework" type="text" 
         [(ngModel)] ="favoriteFramework" />
     </label>
+    <button (click)="showFramework()">Show Framework</button>
   `
 })
 export class UserComponent {
-  //Guarda y muestra el nombre del framework que vamos a escribir en el campo de texto
   favoriteFramework = '';
   username = 'youtech';
+
+  showFramework(){
+    alert(this.favoriteFramework);
+  }
 
 }
